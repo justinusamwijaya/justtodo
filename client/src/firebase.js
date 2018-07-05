0140afbf3d3f865b2c20e5798c7953e025749812
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-07-05 07:11:02 +0700

    try again on deploying

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 var config = {
-  apiKey: 'AIzaSyBggkaoeSgsd2zH_1cpwGZx90-x37R6zSE',
-  authDomain: 'todolist-31146.firebaseapp.com',
-  databaseURL: 'https://todolist-31146.firebaseio.com',
-  projectId: 'todolist-31146',
-  storageBucket: 'todolist-31146.appspot.com',
-  messagingSenderId: '533162468137'
+  apiKey: 'AIzaSyC8lr-W7i7fBN8rZtq-ZmuKK_b6wCyyxfg',
+  authDomain: 'testest-2bce5.firebaseapp.com',
+  databaseURL: 'https://testest-2bce5.firebaseio.com',
+  projectId: 'testest-2bce5',
+  storageBucket: 'testest-2bce5.appspot.com',
+  messagingSenderId: '930833629790'
 }
 
 export const app = firebase.initializeApp(config)
